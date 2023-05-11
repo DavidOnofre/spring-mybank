54af4642961293ec43230a1e700debb68426e2a2
DavidOnofre <donofre@gmail.com>
2023-05-10 23:30:46 -0500

    commit inicial

---
+++ src/main/java/com/kodigo/springmybank/SpringMybankApplication.java
@@ -0,0 +1,13 @@
+package com.kodigo.springmybank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMybankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMybankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kodigo/springmybank/SpringMybankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodigo.springmybank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMybankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
