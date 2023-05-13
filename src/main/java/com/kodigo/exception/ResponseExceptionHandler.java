ee4a0c5f865536028336a86e230c329d55abe22a
DavidOnofre <donofre@gmail.com>
2023-05-13 03:21:02 -0500

    generate file log

--- src/main/java/com/kodigo/exception/ResponseExceptionHandler.java
+++ src/main/java/com/kodigo/exception/ResponseExceptionHandler.java
@@ -1,6 +1,9 @@
 package com.kodigo.exception;
 
+import com.kodigo.controller.ClientController;
 import com.kodigo.util.Constant;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -20,15 +23,19 @@ import java.util.stream.Collectors;
 @RestController
 public class ResponseExceptionHandler extends ResponseEntityExceptionHandler {
 
+    Logger LOGGER = LoggerFactory.getLogger(ResponseExceptionHandler.class);
+
     @ExceptionHandler(Exception.class)
     public final ResponseEntity<ExceptionResponse> handleAllException(Exception ex, WebRequest request) {
         ExceptionResponse er = new ExceptionResponse(LocalDateTime.now(), ex.getMessage(), request.getDescription(false));
+        proccesLog(ex.getMessage(), ex.getCause().getMessage());
         return new ResponseEntity<ExceptionResponse>(er, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(ModelNotFoundException.class)
     public final ResponseEntity<ExceptionResponse> handleModelNotFoundException(ModelNotFoundException ex, WebRequest request) {
         ExceptionResponse er = new ExceptionResponse(LocalDateTime.now(), ex.getMessage(), request.getDescription(false));
+        proccesLog(ex.getMessage(), ex.getCause().getMessage());
         return new ResponseEntity<ExceptionResponse>(er, HttpStatus.NOT_FOUND);
     }
 
@@ -36,6 +43,7 @@ public class ResponseExceptionHandler extends ResponseEntityExceptionHandler {
     public final ResponseEntity<ExceptionResponse> handleDataIntegrityViolationException(DataIntegrityViolationException ex, WebRequest request) {
         String[] causes = getCauses(ex);
         ExceptionResponse er = new ExceptionResponse(LocalDateTime.now(), getMessage(causes), getDetail(causes));
+        proccesLog(ex.getCause().getLocalizedMessage(), ex.getCause().getCause().getMessage());
         return new ResponseEntity<ExceptionResponse>(er, HttpStatus.CONFLICT);
     }
 
@@ -74,7 +82,14 @@ public class ResponseExceptionHandler extends ResponseEntityExceptionHandler {
                 .collect(Collectors.joining());
 
         ExceptionResponse er = new ExceptionResponse(LocalDateTime.now(), aloneMessage, request.getDescription(false));
+
+        proccesLog(aloneMessage, ex.getMessage());
         return new ResponseEntity<Object>(er, HttpStatus.BAD_REQUEST);
+    }
+
+    private void proccesLog(String message, String details) {
+        LOGGER.warn(message);
+        LOGGER.error(details);
     }
 
 }
